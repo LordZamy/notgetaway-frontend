8e948bb6f6e399cd2772ca615f53ecb3818eb0bb
Samarth Wahal <samarthwahal@hotmail.com>
2017-10-22 04:27:43 -0500

    Complete stream players

--- src/StreamContainer.js
+++ src/StreamContainer.js
@@ -1,7 +1,9 @@
+// @flow
 import React, { Component } from 'react';
 import Slider from 'material-ui/Slider';
 import FloatingActionButton from 'material-ui/FloatingActionButton';
 import AvPlayArrow from 'material-ui/svg-icons/av/play-arrow';
+import AvPause from 'material-ui/svg-icons/av/pause';
 
 import StreamPlayer from './StreamPlayer';
 
@@ -11,23 +13,27 @@ type State = {
   height: number,
   width: number,
   seekValue: number,
+  playing: boolean,
+  seeking: boolean,
 }
+
+const PLAYER_COUNT = 3;
 
 const playerButtonStyles = {float: 'left'};
 const sliderStyles = {width: '90%', float:'right'};
 
 class StreamContainer extends Component {
-  containerNode: null;
+  containerNode = null;
 
-  state: State;
+  // references to stream player dom nodes
+  players = [null, null, null];
 
-  constructor(props: Props) {
-    super(props);
-    this.state = {
-      height: 0,
-      width: 0,
-      seekValue: 0,
-    }
+  state: State = {
+    height: 0,
+    width: 0,
+    seekValue: 0,
+    playing: false,
+    seeking: false,
   }
 
   componentDidMount() {
@@ -35,26 +41,34 @@ class StreamContainer extends Component {
     this.setState({height, width});
   }
 
+  renderPlayPauseIcon(playing: boolean): ReactElement<*> {
+    return playing ? <AvPause/> : <AvPlayArrow />;
+  }
+
   render() {
     const playerWidth = this.state.width / 3.5;
     const playerHeight = window.innerHeight / 4;
 
+    const { playing } = this.state;
+
     return (
       <div className="stream-container">
         <div className="stream-player-container" ref={node => this.containerNode = node}>
-          <StreamPlayer height={playerHeight} width={playerWidth} name="Camera 1" />
-          <StreamPlayer height={playerHeight} width={playerWidth} name="Camera 2" />
-          <StreamPlayer height={playerHeight} width={playerWidth} name="Camera 3" />
+          <StreamPlayer height={playerHeight} width={playerWidth} name="Camera 1" onReady={this.onPlayerReady} playing={playing} updateParentRef={this.updatePlayerRef(0)} onProgress={this.onProgress} />
+          <StreamPlayer height={playerHeight} width={playerWidth} name="Camera 2" onReady={this.onPlayerReady} playing={playing} muted={true} updateParentRef={this.updatePlayerRef(1)} />
+          <StreamPlayer height={playerHeight} width={playerWidth} name="Camera 3" onReady={this.onPlayerReady} playing={playing} muted={true} updateParentRef={this.updatePlayerRef(2)} />
         </div>
         <div className="stream-controls-container">
-          <FloatingActionButton style={playerButtonStyles}>
-            <AvPlayArrow />
+          <FloatingActionButton style={playerButtonStyles} onClick={this.onPlayPauseClick}>
+            {this.renderPlayPauseIcon(playing)}
           </FloatingActionButton>
           <Slider
             min={0}
             max={1}
-            value={this.props.seekValue}
+            value={this.state.seekValue}
             onChange={this.onSeekChange}
+            onDragStart={this.onDragStart}
+            onDragStop={this.onDragStop}
             sliderStyle={sliderStyles}
           />
         </div>
@@ -65,6 +79,45 @@ class StreamContainer extends Component {
   // update seek value on slider
   onSeekChange = (e: Object, newValue: number) => {
     this.setState({seekValue: newValue});
+  }
+
+  onDragStart = (e: Object) => {
+    this.setState({seeking: true});
+  }
+
+  onDragStop = (e: Object) => {
+    this.players.forEach(player => player.seekTo(this.state.seekValue));
+    this.setState({seeking: false});
+  }
+
+  onPlayerReady = playerReadyFn(() => {
+    this.setState({playing: true});
+  });
+
+  onPlayPauseClick = (e: Object) => {
+    this.setState({playing: !this.state.playing});
+  }
+
+  onProgress = (progress) => {
+    if (!this.state.seeking) {
+      console.log(progress);
+      this.setState({seekValue: progress.played});
+    }
+  }
+
+  updatePlayerRef = (index: number) => {
+    return (player) => this.players[index] = player;
+  }
+}
+
+// returns a function that can be passed to the StreamPlayer component
+function playerReadyFn(callback) {
+  let numReadyPlayers = 0;
+  return () => {
+    numReadyPlayers++;
+    if (numReadyPlayers === PLAYER_COUNT) {
+      setTimeout(callback, 1000);
+    }
   }
 }
 
--- src/StreamPlayer.js
+++ src/StreamPlayer.js
@@ -1,26 +1,60 @@
+// @flow
 import React, { Component } from 'react';
 import ReactPlayer from 'react-player';
 
-type Props = {
+type DefaultProps = {
+  controls: boolean,
+  onReady: Function,
+  playing: boolean,
+  muted: boolean,
+  updateParentRef: Function,
+  onProgress: Function,
+}
+
+type Props = DefaultProps & {
   height: number,
   width: number,
   name: string,
 }
 
 class StreamPlayer extends Component {
+  static defaultProps: DefaultProps = {
+    controls: false,
+    onReady: () => {},
+    playing: false,
+    updateParentRef: () => {},
+    onProgress: () => {},
+    muted: false,
+  }
+
   props: Props;
 
   render() {
-    const {height, width, name} = this.props;
+    const {
+      controls,
+      height,
+      name,
+      onReady,
+      playing,
+      width,
+      updateParentRef,
+      onProgress,
+      muted
+    } = this.props;
 
     return (
       <div className="stream-player">
         <span className="stream-player-name">{name}</span>
         <ReactPlayer
-          url='https://www.youtube.com/watch?v=ysz5S6PUM-U'
-          playing={false}
+          ref={updateParentRef}
+          controls={controls}
           height={height}
+          onReady={onReady}
+          onProgress={onProgress}
+          playing={playing}
+          url='https://www.youtube.com/watch?v=nANXA3CsUpY'
           width={width}
+          muted={muted}
         />
       </div>
     )
